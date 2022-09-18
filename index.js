879bbe4dda0f787955c5c583b3dc5b1c6b534038
“Ahmed <aymnamard12@gmail.com>
2022-09-18 05:24:57 +0200

    help

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const shortid = require("shortid");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+const originalUrls = [];
+const shortUrls = [];
+
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+  const id = shortid.generate();
+
+  const foundIndex = originalUrls.indexOf(url);
+
+  if (!url.includes("https://") && !url.includes("http://")) {
+    return res.json({ error: "invalid url" });
+  }
+
+  if (foundIndex < 0) {
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length);
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length - 1,
+    });
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex],
+  });
+});
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shorturl = parseInt(req.params.shorturl);
+  const foundIndex = shortUrls.indexOf(shorturl);
+
+  if (foundIndex < 0) {
+    return res.json({
+      error: "no url for that input",
+    });
+  }
+
+  res.redirect(originalUrls[foundIndex]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
